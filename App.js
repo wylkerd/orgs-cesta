9dfdac2755e77839e37880ed79526034cc925cc5
wylkerd <wylkerd@hotmail.com>
2022-12-14 16:28:27 -0300

    feat: App loading with expo-splash-screen

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { 
   StatusBar, 
   SafeAreaView, // Assegura para o IOS uma formatação que não quebra a barra de status  superior e inferior
@@ -11,14 +12,22 @@ import {
 import Cesta from './src/telas/Cesta';
 import mock from './src/mocks/cesta'
 
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-
   // the hook uses Font.loadAsync
   const [fontesCarregadas] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
     "MontserratBold": Montserrat_700Bold
   });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontesCarregadas) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontesCarregadas]);
 
   if (!fontesCarregadas) {
     return <View />;
@@ -27,7 +36,7 @@ export default function App() {
   // utilizando spread operator (...) 
   // para desestruturar o objeto e passar as chaves da desestruturação como se fossem parâmetros da cesta (topo e detalhes).
   return (
-    <SafeAreaView>
+    <SafeAreaView onLayout={onLayoutRootView}>
       <StatusBar />
       <Cesta {...mock} />
     </SafeAreaView>
