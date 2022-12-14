f27bc3b781ca92bcd5acac4891a4c218c5c294ae
wylkerd <wylkerd@hotmail.com>
2022-12-14 13:35:35 -0300

    feat: Added external font Montserrat by comand ' npx expo install expo-font @expo-google-fonts/inter '

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { 
   StatusBar, 
-  SafeAreaView // Assegura para o IOS uma formatação que não quebra a barra de status  superior e inferior
+  SafeAreaView, // Assegura para o IOS uma formatação que não quebra a barra de status  superior e inferior
+  View
 } from 'react-native';
 import Cesta from './src/telas/Cesta';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold 
+} from '@expo-google-fonts/montserrat' // 'npx expo install expo-font @expo-google-fonts/nome_da_fonte', ou instale outra manualmente
 
 export default function App() {
+
+  // the hook uses Font.loadAsync
+  const [fontesCarregadas] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  });
+
+  if (!fontesCarregadas) {
+    return <View />;
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
