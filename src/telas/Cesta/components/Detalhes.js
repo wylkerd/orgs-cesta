725632ab2e7b59531dc46b012de256c7ac5b829b
wylkerd <wylkerd@hotmail.com>
2022-12-14 19:20:41 -0300

    feat: Botao component

--- src/telas/Cesta/components/Detalhes.js
+++ src/telas/Cesta/components/Detalhes.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 import Texto from '../../../components/Texto';
+import Botao from '../../../components/Botao';
 
 export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
 
@@ -17,9 +18,7 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
       <Texto style={estilos.descricao}>{ descricao }</Texto>
       <Texto style={estilos.preco}>{ preco }</Texto>
 
-      <TouchableOpacity style={estilos.botao} onPress={() => {}}>
-        <Texto style={estilos.textoBotao}>{ botao }</Texto>
-      </TouchableOpacity>
+      <Botao texto={ botao } style={ estilos.botao } onPress={() => {}}/>
     </>
   )
 }
@@ -57,16 +56,6 @@ const estilos = StyleSheet.create({
     marginTop: 8
   },
   botao: {
-    marginTop: 16,
-    backgroundColor: '#2A9F85',
-    paddingVertical: 16,
-    borderRadius: 6,
-  },
-  textoBotao: {
-    textAlign: 'center',
-    color: '#ffffff',
-    fontSize: 16,
-    lineHeight: 26,
-    fontWeight: 'bold'
-  },
+    marginTop: 16
+  }
 })
