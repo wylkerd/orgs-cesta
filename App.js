1d8dba162d4fd88c1b9acccea6a6f0bbc5ea69a0
wylkerd <wylkerd@hotmail.com>
2022-12-14 11:26:55 -0300

    Adicionada imagens e titulo

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StatusBar, 
+  SafeAreaView // Assegura para o IOS uma formatação que não quebra a barra de status  superior e inferior
+} from 'react-native';
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Alura</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/telas/Cesta.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Image, StyleSheet, Dimensions, Text } from 'react-native';
+
+import topo from '../../assets/topo.png'
+
+const width = Dimensions.get('screen').width
+
+export default function Cesta() {
+
+  return (
+    <>
+      <Image source={topo} style={estilos.topo}/>
+      <Text style={estilos.titulo}>Detalhe da cesta</Text>
+    </>
+  )
+}
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: '100%',
+    height: 578 / 768 * width, // Altura da imagem / Largura da imagem * width da tela
+  },
+  titulo: {
+    width: '100%',
+    position: 'absolute',
+    textAlign: 'center',
+    fontSize: 16,
+    lineHeight: 26,
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 16
+  }
+})
